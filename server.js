65a0c05c25b12141ba503b7eb65819f1b4c772be
vijaysidda <47773989+vijaysidda@users.noreply.github.com>
2019-10-20 21:28:47 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/myShopping'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/my-shopping'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/myShopping/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/my-shopping/index.html'));
 });
 
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
